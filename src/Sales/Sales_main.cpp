8dcfb1266b50c7f4f5c0ec3307a2bf62b947d21c
Zilin Zheng <wsnzg6@outlook.com>
2026-04-04 10:57:56 +0800

    -

--- src/Sales/Sales_main.cpp
+++ src/Sales/Sales_main.cpp
@@ -4,29 +4,30 @@
  */
 
 #include <iostream>
-#include "Sales_item.h"
+#include "Sales_data.h"
 
 using namespace std;
 
 int main() {
-    Sales_item total; // 保存和的变量
+    Sales_data total; // 保存和的变量
 
     // 读入第一笔交易，并确保有数据可以处理
-    if (cin >> total) {
-        Sales_item trans;
+    if (read(cin, total)) {
+        Sales_data trans;
+        cout << "<ISBN> <units_sold> <price>" << endl; // 提示输入格式
 
         // 读入剩余的交易
-        while (cin >> trans) {
+        while (read(cin, trans)) {
             // 如果我们仍在处理相同的书
             if (total.isbn() == trans.isbn())
-                total += trans; // 更新总销售额
+                total.combine(trans); // 更新总销售额
             else {
                 // 输出前一本书的结果
-                cout << total << endl;
+                print(cout, total) << endl; // 打印结果并换行
                 total = trans; // total现在表示下一本书的销售额
             }
         }
-        cout << total << endl; // 输出最后一本书的结果
+        print(cout, total) << endl; // 输出最后一本书的结果
     } else {
         // 没有输入，给出提示
         cerr << "No data?!" << endl;
